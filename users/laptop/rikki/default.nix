612f704846b0d8a6db318ae3f26d4d5cb29c2c96
Rikki <me@rikki.pub>
2024-09-26 14:29:57 +0800

    load user plugin: blesh

--- users/laptop/rikki/default.nix
+++ users/laptop/rikki/default.nix
@@ -1,7 +1,12 @@
 {
+  imports = [
+    ./plugin/blesh
+  ];
+
   users.users.rikki = {
     isNormalUser = true;
     extraGroups = [ "wheel" "vboxusers" ];
   };
+
   home-manager.users.rikki = import ./rikki.nix;
 }
