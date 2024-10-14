fc22380417543cbe58f199b632dcabde4298a436
Rikki <me@rikki.pub>
2024-10-14 18:27:12 +0800

    feat: change bash to fish

--- users/rikki-laptop/layer/shell/default.nix
+++ users/rikki-laptop/layer/shell/default.nix
@@ -1,47 +1,40 @@
 {pkgs, ...}: let
   trackerList = import ./aria2-tracker;
 in {
-  home.packages = with pkgs; [
-    # TODO : waiting for new merge
-    blesh
-  ];
-
-  # 启用 starship，这是一个漂亮的 shell 提示符
-  programs.starship = {
+  programs.fish = {
     enable = true;
-    # 自定义配置
-    settings = {
-      add_newline = true;
-      character = {
-        success_symbol = "[>](bold green)";
-        error_symbol = "[x](bold red)";
-        vimcmd_symbol = "[<](bold green)";
-      };
-    };
-  };
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
 
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin"
       export TL=${trackerList}
-
-      # ble.sh loader
-      source "$(blesh-share)"/ble.sh --attach=none # does not work currently
-      [[ ! $\{BLE_VERSION-\} ]] || ble-attach
-
       0file() { curl -F"file=@$1" https://envs.sh ; }
       0pb() { curl -F"file=@-;" https://envs.sh ; }
       0url() { curl -F"url=$1" https://envs.sh ; }
       0short() { curl -F"shorten=$1" https://envs.sh ; }
     '';
-
-    shellAliases = {
-      nixos-update = ''
-        sudo systemctl stop nixos-rebuild-switch-to-configuration.service
-        sudo nixos-rebuild switch --flake /home/rikki/WorkSpace/Dev/NixOS-Config/#ASUS_TianXuan4_Rikki
-      '';
-    };
+    plugins = [
+      {
+        name = "pure";
+        src = pkgs.fishPlugins.pure.src;
+      }
+      {
+        name = "fzf.fish";
+        src = pkgs.fetchFromGitHub {
+          owner = "PatrickF1";
+          repo = "fzf.fish";
+          rev = "e5d54b93cd3e096ad6c2a419df33c4f50451c900";
+          hash = "sha256-5cO5Ey7z7KMF3vqQhIbYip5JR6YiS2I9VPRd6BOmeC8=";
+        };
+      }
+      {
+        name = "fish-async-prompt";
+        src = pkgs.fetchFromGitHub {
+          owner = "acomagu";
+          repo = "fish-async-prompt";
+          rev = "316aa03c875b58e7c7f7d3bc9a78175aa47dbaa8";
+          hash = "sha256-J7y3BjqwuEH4zDQe4cWylLn+Vn2Q5pv0XwOSPwhw/Z0=";
+        };
+      }
+    ];
   };
 }
