07a8a3780519122c55902a27bc13f6ce2d7df7d1
Rikki <me@rikki.pub>
2024-11-17 03:51:59 +0800

    enhance: Improve examples and documentation.

--- plugin/user-shell/default.nix
+++ plugin/user-shell/default.nix
@@ -7,12 +7,24 @@ let
     bashrcExtra = mkOption {
       type = types.lines;
       default = "";
+      example = ''
+        export username=rikki
+      '';
+      description = ''
+        Extra commands that should be placed in {file}~/.bashrc.
+        Note that these commands will be run even in non-interactive shells.
+        (Inherit from parent)
+      '';
     };
 
     blesh = {
       enable = mkOption {
         type = with types; bool;
         default = false;
+        example = false;
+        description = ''
+          Choose whether to enable the blesh feature.
+        '';
       };
     };
 
@@ -20,6 +32,10 @@ let
       enable = mkOption {
         type = with types; bool;
         default = false;
+        example = false;
+        description = ''
+          Choose whether to enable the starship feature.
+        '';
       };
     };
   };
@@ -31,11 +47,19 @@ let
         enable = mkOption {
           type = with types; bool;
           default = false;
+          example = false;
+          description = ''
+            Choose whether to enable the users-shell feature for a specific user.
+          '';
         };
 
         defaultShell = mkOption {
           type = types.str;
           default = "bash";
+          example = "fish";
+          description = ''
+            Select the default shell environment for a specific user.
+          '';
         };
 
         # load shell options
@@ -71,6 +95,7 @@ in
     variables = {
       tracker-aria2 = mkOption {
         type = with types; str;
+        default = "";
         example = "udp://a:1337/announce,udp://b:1337/announce,...";
         description = ''
           A string of tracker URLs separated by commas.
