f0187159df4599f2b48f260b48103491897e6443
Rikki <me@rikki.pub>
2024-11-26 01:29:43 +0800

    feat: alejandra options support

--- plugin/user-shell/users/plugin/alejandra/default.nix
+++ plugin/user-shell/users/plugin/alejandra/default.nix
@@ -1,4 +1,30 @@
-{ alejandra, pkgs, ... }:
 {
-  home.packages = [ alejandra.defaultPackage.${pkgs.stdenv.hostPlatform.system} ];
+  alejandra,
+  lib,
+  config,
+  pkgs,
+  system ? pkgs.stdenv.hostPlatform.system,
+  ...
+}:
+let
+  inherit (lib) mapAttrs mkIf elem;
+
+  root = config.user-shell;
+  cfg = root.users;
+
+  plugin-conf =
+    name: value:
+    let
+      # inner cfg, helps function to locate specific configs
+      icfg = value.languageServer;
+    in
+    mkIf (elem "alejandra" icfg) { home.packages = [ alejandra.defaultPackage.${system} ]; };
+
+  # Check whether the `user-shell` module is enabled for this user.
+  isEnabled = v: v.enable;
+in
+{
+  config = {
+    home-manager.users = mapAttrs (name: v: mkIf (isEnabled v) (plugin-conf name v)) cfg;
+  };
 }
