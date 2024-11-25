6459d9360da225ad0770fd85b2436dd726ae9630
Rikki <me@rikki.pub>
2024-11-26 01:29:34 +0800

    feat: nil options support

--- plugin/user-shell/users/plugin/nil/default.nix
+++ plugin/user-shell/users/plugin/nil/default.nix
@@ -1,4 +1,30 @@
-{ nil, pkgs, ... }:
 {
-  home.packages = [ nil.packages.${pkgs.stdenv.hostPlatform.system}.default ];
+  nil,
+  lib,
+  config,
+  pkgs,
+  system ? pkgs.stdenv.hostPlatform.system,
+  ...
+}:
+let
+  inherit (lib) mapAttrs mkIf elem;
+
+  root = config.user-shell;
+  cfg = root.users;
+
+  plugin-conf =
+    name: value:
+    let
+      # inner cfg, helps function to locate specific configs
+      icfg = value.languageServer;
+    in
+    mkIf (elem "nil" icfg) { home.packages = [ nil.packages.${system}.default ]; };
+
+  # Check whether the `user-shell` module is enabled for this user.
+  isEnabled = v: v.enable;
+in
+{
+  config = {
+    home-manager.users = mapAttrs (name: v: mkIf (isEnabled v) (plugin-conf name v)) cfg;
+  };
 }
