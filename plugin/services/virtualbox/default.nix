5b699e5055d9c8e4aa67a48c955936329a4769d1
Rikki <me@rikki.pub>
2025-02-05 18:27:37 +0800

    fix: overlay virtualbox because https://github.com/NixOS/nixpkgs/issues/378891

--- plugin/services/virtualbox/default.nix
+++ plugin/services/virtualbox/default.nix
@@ -1,14 +1,27 @@
 # if you want to enable this config
 # you need to add "vboxusers" to users.users.<name>.extraGroups
 {
-  virtualisation.virtualbox = {
-    host = {
-      enable = true;
-      enableKvm = true;
-      addNetworkInterface = false;
+  virtualisation.virtualbox =
+    let
+      pkgs = import (builtins.fetchGit {
+        # Descriptive name to make the store path easier to identify
+        name = "my-old-revision";
+        url = "https://github.com/NixOS/nixpkgs/";
+        ref = "refs/heads/nixos-24.05";
+        rev = "6eb01a67e1fc558644daed33eaeb937145e17696";
+      }) { };
+
+      overlay-virtualbox = pkgs.virtualbox;
+    in
+    {
+      host = {
+        enable = true;
+        enableKvm = true;
+        addNetworkInterface = false;
+        package = overlay-virtualbox;
+      };
+      guest = {
+        enable = true;
+      };
     };
-    guest = {
-      enable = true;
-    };
-  };
 }
