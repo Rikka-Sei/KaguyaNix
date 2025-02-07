cad9a7ac9cdf094d0232a1e486513d2895b87328
Rikki <me@rikki.pub>
2025-02-07 14:45:17 +0800

    patch: specify `programs.sqlite` for `command-not-found` from flakes

--- plugin/default.nix
+++ plugin/default.nix
@@ -3,9 +3,9 @@
   imports = [
     inputs.nix-flatpak.nixosModules.nix-flatpak
     inputs.home-manager.nixosModules.home-manager
-    inputs.nix-ld.nixosModules.nix-ld
     ./font
     ./input
+    ./patch
     ./user-environment
   ];
 
---
+++ plugin/patch/default.nix
@@ -0,0 +1,6 @@
+{ ... }:
+{
+  imports = [
+    ./command-not-found.nix
+  ];
+}
