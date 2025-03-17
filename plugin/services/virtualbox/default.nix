f8d9b85a399cd47148eeb14597d69cf5d463f69b
Rikki <me@rikki.pub>
2025-03-18 05:34:13 +0800

    refactor: Simplify installation code due to upstream VirtualBox issue resolution

--- plugin/services/virtualbox/default.nix
+++ plugin/services/virtualbox/default.nix
@@ -1,27 +1,8 @@
 # if you want to enable this config
 # you need to add "vboxusers" to users.users.<name>.extraGroups
 {
-  virtualisation.virtualbox =
-    let
-      pkgs = import (builtins.fetchGit {
-        # Descriptive name to make the store path easier to identify
-        name = "overlay-virtualbox";
-        url = "https://github.com/NixOS/nixpkgs/";
-        ref = "refs/heads/nixos-24.05";
-        rev = "6eb01a67e1fc558644daed33eaeb937145e17696";
-      }) { };
-
-      overlay-virtualbox = pkgs.virtualbox;
-    in
-    {
-      host = {
-        enable = true;
-        enableKvm = true;
-        addNetworkInterface = false;
-        package = overlay-virtualbox;
-      };
-      guest = {
-        enable = true;
-      };
-    };
+  virtualisation.virtualbox.host.enable = true;
+  users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+  virtualisation.virtualbox.guest.enable = true;
+  virtualisation.virtualbox.guest.dragAndDrop = true;
 }
