3ceba78f8b4ab2804656acb4a644ec553498e68f
Rikki <me@rikki.pub>
2025-03-18 05:12:04 +0800

    refactor: some changes

--- plugin/services/vm/default.nix
+++ plugin/services/vm/default.nix
@@ -1,16 +1,24 @@
 { pkgs, ... }:
 {
-  virtualisation = {
-    libvirtd = {
-      enable = true;
-      qemu = {
-        verbatimConfig = ''
-          nvram = [ "${pkgs.OVMF}/FV/OVMF.fd:${pkgs.OVMF}/FV/OVMF_VARS.fd" ]
-        '';
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
       };
     };
-    spiceUSBRedirection.enable = true;
   };
+
+  virtualisation.spiceUSBRedirection.enable = true;
 
   environment = {
     systemPackages = with pkgs; [
