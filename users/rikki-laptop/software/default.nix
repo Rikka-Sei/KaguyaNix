3fd6bbd3857ad521fa9b9d90835d0b480d8b8c7c
Rikki <me@rikki.pub>
2025-03-18 05:34:58 +0800

    feat: Add QQ, Cursor, and GnuPG software

--- users/rikki-laptop/software/default.nix
+++ users/rikki-laptop/software/default.nix
@@ -44,5 +44,13 @@
       # Generater
       typst
       thunderbird
+      qq
+      code-cursor
     ];
+
+  programs.gpg.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
+  };
 }
