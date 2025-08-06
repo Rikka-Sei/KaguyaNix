3da071fe48fcede1e16828c3868a04e0678edb38
Rikki <me@rikki.pub>
2025-08-06 15:22:48 +0800

    fix: 修复libvirt不支持多人使用的问题

--- modules/services/vm.nix
+++ modules/services/vm.nix
@@ -1,4 +1,13 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+{
+  # 根据 NixOS Wiki 官方文档的配置
+  programs.virt-manager.enable = true;
+
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
@@ -11,8 +20,7 @@
           (pkgs.OVMF.override {
             secureBoot = true;
             tpmSupport = true;
-          })
-          .fd
+          }).fd
         ];
       };
     };
@@ -27,6 +35,14 @@
       qemu
       OVMF
       gvfs
+      dnsmasq
     ];
   };
+
+  # 动态将系统配置中定义的所有用户添加到 libvirtd 组
+  users.users = lib.mkMerge (
+    lib.mapAttrsToList (username: userCfg: {
+      ${username}.extraGroups = [ "libvirtd" ];
+    }) (config.systemConfig.users or { })
+  );
 }
