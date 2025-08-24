5c1cb6171dddc957d348ddf8a9991d524809288b
Rikki <me@rikki.pub>
2025-08-24 15:24:51 +0800

    fix: 修复gnucash软件添加方式错误问题

--- users/rikki/profiles/business.nix
+++ users/rikki/profiles/business.nix
@@ -1,20 +1,18 @@
 {
   pkgs,
-  inputs,
   ...
 }:
 let
   userName = "rikki";
 in
 {
-  # 开发工具
+  # 系统软件
   environment.systemPackages = [
   ];
 
   home-manager.users.${userName} = {
-    programs.gnucash.enable = true;
+    home.packages = with pkgs; [
+      gnucash
+    ];
   };
-
-  users.users.${userName}.shell = pkgs.fish;
-  programs.fish.enable = true;
 }
