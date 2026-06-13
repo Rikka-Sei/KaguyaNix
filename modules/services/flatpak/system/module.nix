fc3058d63acba063d5191a2cbc2fd95be6cc0664
Rikki <me@rikki.pub>
2026-06-13 09:05:08 +0800

    feat(flatpak): move package list from module default to system override
    Packages default changed from hardcoded 28-item list to empty [].
    Each system now declares its Flatpak packages in its meta.nix overrides,
    making the module a pure mechanism with no embedded host-specific config.

--- modules/services/flatpak/system/module.nix
+++ modules/services/flatpak/system/module.nix
@@ -19,36 +19,7 @@ in {
     };
     packages = lib.mkOption {
       type = lib.types.listOf lib.types.str;
-      default = [
-        "ar.xjuan.Cambalache"
-        "com.google.Chrome"
-        "com.obsproject.Studio"
-        "com.valvesoftware.Steam"
-        "com.tencent.WeChat"
-        "dev.geopjr.Calligraphy"
-        "org.gnome.Boxes"
-        "org.gnome.Builder"
-        "org.gnome.GHex"
-        "org.inkscape.Inkscape"
-        "org.kicad.KiCad"
-        "org.libreoffice.LibreOffice"
-        "org.octave.Octave"
-        "org.qbittorrent.qBittorrent"
-        "org.sdrangel.SDRangel"
-        "org.telegram.desktop"
-        "org.telegram.desktop.webview"
-        "re.sonny.Workbench"
-        "org.gnome.Fractal"
-        "org.gnome.World.Secrets"
-        "org.blender.Blender"
-        "com.tencent.wemeet"
-        "com.baidu.NetDisk"
-        "io.github.Foldex.AdwSteamGtk"
-        "org.gabmus.gfeeds"
-        "com.belmoussaoui.Authenticator"
-        "com.github.tchx84.Flatseal"
-        "com.qq.QQ"
-      ];
+      default = [];
       description = "Flatpak 默认安装列表。";
     };
   };
